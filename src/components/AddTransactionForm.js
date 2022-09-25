59e45e3a1b4e473c262db5df062e70b5d82288bb
ogola5 <ogolaevance5@gmail.com>
2022-09-25 22:10:32 +0300

    updated

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,9 +1,31 @@
-import React from "react";
+import { application } from "express";
+import React, { useState } from "react";
 
 function AddTransactionForm() {
+  const[formData,setFormData]= useState({
+    date:"",
+    description:"",
+    category:"",
+    amount:null
+  })
+  function handleChange(event){
+    setFormData({
+      ...formData,
+      [event.target.name]:event.target.value,
+    })
+  }
+  function handleSubmit(event){
+    event.preventDefault()
+    fetch("http://localhost:8001/transactions",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },body:JSON.stringify(formData),
+    })
+  }
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={handleSubmit} className="ui form">
         <div className="inline fields">
           <input type="date" name="date" />
           <input type="text" name="description" placeholder="Description" />
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 function Transaction() {
+  const [Transaction,setTransactions]=useState([])
+  useEffect(()=>{
+    fetch("http://localhost:8001/transactions")
+    .then((resp) =>resp.json())
+    .then((data)=>{
+      setTransactions(data)
+    })
+  },[])
+  const eachTransaction= transaction.map((single)=>{
+    return(
+      <>
+      <table>
+        <tr>
+          <td>{single.date}</td>
+          <td>{single.description}</td>
+          <td>{single.category}</td>
+          <td>{single.amount}</td>
+        </tr>
+      </table>
+      </>
+
+    )
+      
+  })
   return (
-    <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-    </tr>
+    <div>
+    {eachTransaction}
+  </div>
   );
 }
 
